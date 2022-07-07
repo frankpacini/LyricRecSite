bdd42ced5d04c198f9408f97433966ea62d45da4
Frank Pacini <fgpacini@yahoo.com>
2022-07-06 17:25:59 -0700

    added search

--- server/lyricrec/views.py
+++ server/lyricrec/views.py
@@ -1,5 +1,8 @@
+import requests
+from sqlite3 import DataError
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.forms.models import model_to_dict
 
 from .models import Track
 from .scraper import Scraper
@@ -19,4 +22,31 @@ def index(request, id):
             track = Track(**song_dict)
             track.save()
     return HttpResponse("{} by {}\n\n{}".format(track.title, track.artist, track.lyrics))
-        
+
+
+session = requests.Session()
+session.headers = {
+    'application': 'LyricRec',
+    'User-Agent': 'https://github.com/frankpacini/LyricRec'
+}
+access_token = 'Bearer ' + open('../creds.txt').read().split('\n')[4]
+authorization_header = {'Authorization': access_token}
+
+def search(request, query):
+    search_response = session.request('GET', 'https://api.genius.com/search/', params={'q': query, "per_page": 10}, headers=authorization_header)
+    search_results = search_response.json()['response']['hits']
+
+    parsed_results = []
+    for result in search_results:
+        if result['type'] != 'song':
+            raise DataError
+        result = result['result']
+        parsed_result = {
+                            'id': result['id'],
+                            'title': result['title'],
+                            'artist': result['primary_artist']['name'],
+                            'image_thumbnail_url': result['song_art_image_thumbnail_url']
+                        }
+        parsed_results.append(parsed_result)
+
+    return JsonResponse({'results': parsed_results})
